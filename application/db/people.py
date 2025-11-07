e55876d5adec28ac13051bae218737477b75303d
znst5 <161199179+znst5@users.noreply.github.com>
2025-11-07 07:27:57 +0300

    qu

--- application/db/people.py
+++ application/db/people.py
@@ -1,1 +1,15 @@
+import emoji
 
+class Employee:
+    def __init__(self, name, age, position, bonus):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.bonus = bonus
+    def __str__(self):
+        return f'(Имя:{self.name}, Возраст:{self.age}, Должность:{self.position}, Премия:{self.bonus})'
+
+
+if __name__ == '__main__':
+    e = Employee('Data', 23, 'worker', 1000)
+    print(e, emoji.emojize(':red_heart: :man_office_worker:'))
