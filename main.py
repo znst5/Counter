fef0d29ba4faa9e877ccfa6f089222c0eec2e043
znst5 <161199179+znst5@users.noreply.github.com>
2025-11-10 12:54:27 +0300

    init

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from datetime import datetime, date, time
+import emoji
+from application.salary import Salary
+from application.db.people import Employee
 
-print(datetime.now())
+s = Salary('Data', 23, 'worker', 1000, 13, 1000 )
+print(f'{s.name} has salary {s.get_salary()} on {datetime.now()}', emoji.emojize(':woman_dancing: :man_dancing:'))
